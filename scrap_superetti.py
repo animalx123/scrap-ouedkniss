c7eaaaf279c24a32d1822e87df1ab677d5b93b2c
unknown <anis-gg@hotmail.com>
2021-09-20 11:46:49 +0100

    v2

--- scrap_superetti.py
+++ scrap_superetti.py
@@ -31,25 +31,55 @@ for i in cat:
 
 
 #parcourir la pagination des categories
+start_time = time.perf_counter ()
+nbr_produit = 1
+with open('produit_sup.csv','w') as line:
+
+    for ind_cat, lien_categorie in enumerate(urls_cats):
+
+        cond = True
+        nb_page = 1
+        
+        while cond :
+
+            driver.get(lien_categorie+'/page/'+str(nb_page)+'/')
 
 
-for ind_cat, lien_categorie in enumerate(urls_cats):
+            nb_page += 1
 
-    cond = True
-    nb_page = 1
-    
-    while cond :
+            try:
+                if  driver.find_element_by_css_selector('#modal-ready > div > div > div > section > div > div > div.elementor-column.elementor-col-66.elementor-top-column.elementor-element.elementor-element-7301229 > div > div > div.elementor-element.elementor-element-3742262.elementor-widget.elementor-widget-wd_title > div > div > div.liner-continer > h4'):
+                    print('404  détecter.....................................')
+                    cond = False
+            except:
+                items = driver.find_elements_by_css_selector('div.product-grid-item')
+                categorie = driver.find_element_by_class_name('entry-title').text
 
-        driver.get(lien_categorie+'/page/'+str(nb_page)+'/')
+                for ind_produit,i in enumerate (items):
+                    try:
+                        titre = i.find_element_by_class_name('product-title').find_element_by_tag_name('a').text
+                    except:
+                        titre= ''
+                        print('titre ignorée ...........................')
+                    try:
 
+                        prix = i.find_element_by_class_name('price').find_element_by_tag_name('span').text
+                    except:
+                        prix = ''
+                        print('prix ignorée .................')
+                    try:
+                        image = i.find_element_by_class_name('product-element-top').find_element_by_tag_name('img').get_attribute('data-src')
+                    except:
+                        image = ''
+                        print('image ignorée ...........................')
+                    li = [titre,prix,categorie,image]
+                    li=';'.join(li)
 
-        nb_page += 1
+                    try:
+                        line.write(li+ '\n')
+                        nbr_produit += 1 
+                        print('Produit N:'+str(nbr_produit)+' Ajoutée ...................')
+                    except:
+                        print('Produit N:'+str(nbr_produit)+' Ingnorée ...................')
 
-        try:
-            if  driver.find_element_by_css_selector('#modal-ready > div > div > div > section > div > div > div.elementor-column.elementor-col-66.elementor-top-column.elementor-element.elementor-element-7301229 > div > div > div.elementor-element.elementor-element-3742262.elementor-widget.elementor-widget-wd_title > div > div > div.liner-continer > h4'):
-                print('404  détecter.....................................')
-                cond = False
-        except:
-            print('404 non détecter ........................................')
-
-            
+print(str(nbr_produit)+'Produit récolté en '+str(temp_ecoule(start_time)))
